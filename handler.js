37e5b97f951c9cad3bc3f11bbced66ef8332f0e3
Maiko Morales <maiko.moralesr@gmail.com>
2019-11-29 15:43:04 -0400

    Defining a route for our GET endpoint called greeting

--- handler.js
+++ handler.js
@@ -1,18 +1,14 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.greeting = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'Hello world!'
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
